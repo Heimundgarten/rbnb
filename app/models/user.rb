b7dfef49aff8ce29c47dfeec2ce02be15dd0364e
Selin Immel <selin.immel@whu.edu>
2019-05-28 10:40:30 +0800

    basic implementation of devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :reviews, through: :bookings
 
-  validates :first_name, :last_name, :address, presence: true, allow_blank: false
+  # validates :first_name, :last_name, :address, presence: true, allow_blank: false
 end
