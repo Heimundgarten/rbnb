870e60da846bd0b23c6c9335d403d384be138db9
Nicolas Klug <nicolasklug@gmail.com>
2019-05-28 23:17:09 +0800

    added gem faker and basic seed-structure

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :development, :test do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 end
 
 gem 'dotenv-rails', groups: [:development, :test]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Name.name.capitalize,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories.sample
+  )
+
+  # rand(5..10).times do
+  #   review = Review.create(
+  #     content: Faker::Movies::VForVendetta.quote,
+  #     rating: rand(1..10)
+  #   )
+  # end
+end
+puts 'Finished!'
+
+
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+
+puts 'Creating 100 fake posts...'
+100.times do
+  post = Post.new(
+    title: Faker::Movies::VForVendetta.character,
+    url: Faker::Movies::VForVendetta.speech
+  )
+  post.save!
+end
