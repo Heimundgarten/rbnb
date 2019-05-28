00027b1df19bf1fbc10d198965bbb6140df39fca
Selin Immel <selin.immel@whu.edu>
2019-05-28 12:18:00 +0800

    finished implementing the devise gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,33 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :edit, :destroy]
+  before_action :set_user, only: [:show, :edit, :destroy]
 
-  # def show
-  # end
+  def show
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def update
-  #   @user = User.update(user_params)
-  #   if @user.save
-  #     redirect_to user_path(@user)
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @user = User.update(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
 
-  # def destroy
-  #   @user.destroy
-  #   redirect_to users_path
-  # end
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
 
-  # private
+  private
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-  # def user_params
-  #   params.require(:user).permit(:first_name, :last_name, :address, :photo, :photo_cache)
-  # end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :photo, :photo_cache)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :reviews, through: :bookings
 
-  # validates :first_name, :last_name, :address, presence: true, allow_blank: false
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
 end
