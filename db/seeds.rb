8be1386579c9080f45df4238478e47368fdb0bea
Nicolas Klug <nicolasklug@gmail.com>
2019-05-29 00:49:58 +0800

    first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,94 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 
 puts 'Cleaning database...'
-Restaurant.destroy_all
+# Boardgame.destroy_all
+# Review.destroy_all
+# Booking.destroy_all
+# sleep(2)
+# puts 'Creating user...'
+# 15.times do
+#   user = User.new(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     address: Faker::Address.full_address,
+#     email: Faker::Internet.free_email,
+#     created_at: Faker::Date.backward(54321),
+#     encrypted_password: Faker::Internet.password,
+#     photo: Faker::Placeholdit.image('50x50', 'jpg')
+#   )
+#   user.save!
+# end
 
-puts 'Creating restaurants...'
-categories = ["chinese", "italian", "japanese", "french", "belgian"]
 
+sleep(2)
+puts 'Creating boardgames...'
 10.times do
-  restaurant = Restaurant.create(
-    name: Faker::Name.name.capitalize,
-    address: Faker::Address.full_address,
-    phone_number: Faker::PhoneNumber.phone_number,
-    category: categories.sample
+  boardgame = Boardgame.create(
+    name: Faker::Games::Zelda.game,
+    description: Faker::Movies::VForVendetta.speech,
+    year: Faker::Date.backward(54321),
+    price_per_day: Faker::Number.within(1..10),
+    user_id: rand(1..2),
+    photo: Faker::Placeholdit.image('50x50', 'jpg'),
+    created_at: Faker::Date.backward(54321)
   )
-
-  rand(5..10).times do
-    review = Review.create(
-      content: Faker::Movies::VForVendetta.quote,
-      rating: rand(1..10)
-    )
-  end
+  boardgame.save!
 end
+
+sleep(2)
+puts 'Creating bookings...'
+10.times do
+  booking = Booking.new(
+    total_price: Faker::Number.within(10..30),
+    date_borrowed:Faker::Date.between_except(1.year.ago, 20.days.ago, 21.days.ago),
+    date_returned:Faker::Date.between_except(19.days.ago, 2.days.ago, Date.today),
+    user_id: rand(1..2),
+    boardgame_id: rand(1..3),
+    created_at: Faker::Date.backward(54321)
+  )
+  booking.save!
+end
+
+sleep(2)
+puts 'Creating reviews...'
+rand(4..8).times do
+  review = Review.create(
+    content: Faker::Movies::VForVendetta.quote,
+    rating: rand(1..5),
+    booking_id: rand(1..20)
+  )
+end
+
 puts 'Finished!'
-
-
-require "faker"
-
-# TODO: Write a seed to insert 100 posts in the database
-
-puts 'Creating 100 fake posts...'
-100.times do
-  post = Post.new(
-    title: Faker::Movies::VForVendetta.character,
-    url: Faker::Movies::VForVendetta.speech
-  )
-  post.save!
-end
-
-
-
-  create_table "boardgames", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "year"
-    t.float "price_per_day"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "photo"
-    t.index ["user_id"], name: "index_boardgames_on_user_id"
-  end
-
-  create_table "bookings", force: :cascade do |t|
-    t.float "total_price"
-    t.date "date_borrowed"
-    t.date "date_returned"
-    t.bigint "user_id"
-    t.bigint "boardgame_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["boardgame_id"], name: "index_bookings_on_boardgame_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.text "content"
-    t.integer "rating"
-    t.bigint "booking_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_reviews_on_booking_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
