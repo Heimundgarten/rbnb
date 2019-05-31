d11c403ac7ece1696d1a2c304115424a40d90233
Nicolas Klug <nicolasklug@gmail.com>
2019-05-31 14:20:06 +0800

    set the uploader with version standard and thump and modified the boardview picture of the boardgame

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,13 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  process eager: true  # Force version generation at upload time.
+
+  version :standard do
+    resize_to_fill 400, 300
+  end
+
+  version :thumb do
+    resize_to_fill 100, 100
+  end
 end
