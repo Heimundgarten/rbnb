c68c326af48abbe30434432fa81b915b968718f8
Alina Heimgartner <heimgartner.alina@gmail.com>
2019-05-31 15:10:01 +0800

    updated index page and some css on buttons and the css and backend for bookings page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:show] do
+  resources :bookings  do
     resources :reviews, only: [:create]
   end
 
